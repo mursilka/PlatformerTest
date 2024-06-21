a655e96f60650602fcf80eaa5aec88086dae5d63
SHIKOVECAV\LESHA <mursilka1997@gmail.com>
2024-06-21 15:33:34 +0300

    Исправление ошибок анимации падения. Иправление разварота игрока без нажатия клавишь "A"и"D"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Map_Elements;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -12,28 +9,49 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private int health = 3;
     [SerializeField] private GameUIManager uiManager;
-    [SerializeField] private float repulsiveForce= 10f;
-    
-    private Rigidbody2D rb;
-    private SpriteRenderer sr;
-    private Animator animator;
+    [SerializeField] private float repulsiveForce = 10f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private Animator animator;
+
     private bool isGrounded;
     private int keysCollected = 0;
     private Transform platform;
     private Vector3 platformLastPosition;
+    private bool facingRight = true;
+
+    // Хеши для параметров анимации
+    private readonly int runHash = Animator.StringToHash("Run");
+    private readonly int idleHash = Animator.StringToHash("Idle");
+    private readonly int jumpHash = Animator.StringToHash("Jump");
+    private readonly int fallHash = Animator.StringToHash("Fall");
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
         uiManager.Initialize(health, health);
     }
 
     void Update()
     {
-        Move();
-        Jump();
+        HandleInput();
+        
+        // Проверка на падение
+        if (rb.velocity.y <= 0f && !isGrounded)
+        {
+            animator.SetBool(fallHash, true);
+            animator.SetBool(jumpHash, false);
+            animator.SetBool(runHash, false);
+            animator.SetBool(idleHash, false);
+        }
+        // Проверка на прыжок
+        if (rb.velocity.y > 0f && !isGrounded)
+        {
+            animator.SetBool(fallHash, false);
+            animator.SetBool(jumpHash, true);
+            animator.SetBool(runHash, false);
+            animator.SetBool(idleHash, false);
+        }
+
         if (platform != null)
         {
             Vector3 platformMovement = platform.position - platformLastPosition;
@@ -42,28 +60,51 @@ public class Player : MonoBehaviour
         }
     }
 
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void HandleInput()
+    {
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
     void Move()
     {
         float move = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
-        sr.flipX = move < 0;
+        if (move != 0)
+        {
+            if (move > 0 && !facingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && facingRight)
+            {
+                Flip();
+            }
+        }
 
         if (isGrounded)
         {
-            animator.SetBool("Run", move != 0);
-            animator.SetBool("Idle", move == 0);
+            animator.SetBool(runHash, move != 0);
+            animator.SetBool(idleHash, move == 0);
         }
     }
 
     void Jump()
     {
-        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && isGrounded)
-        {
-            rb.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
-            animator.SetBool("Jump", true);
-            animator.SetBool("Run", false);
-            animator.SetBool("Idle", false);
-        }
+        rb.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);       
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        sr.flipX = !sr.flipX;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -71,15 +112,15 @@ public class Player : MonoBehaviour
         if (collision.gameObject.GetComponent<Ground>() != null)
         {
             isGrounded = true;
-            animator.SetBool("Jump", false);
-            animator.SetBool("Fall", false);
+            animator.SetBool(jumpHash, false);
+            animator.SetBool(fallHash, false);
         }
 
         if (collision.gameObject.GetComponent<Obstacle>() != null)
         {
             Vector2 oppositeDirection = -rb.velocity.normalized;
-            Vector2 force = oppositeDirection + Vector2.up * repulsiveForce; 
-            rb.AddForce(force , ForceMode2D.Impulse); 
+            Vector2 force = oppositeDirection + Vector2.up * repulsiveForce;
+            rb.AddForce(force, ForceMode2D.Impulse);
             health--;
             uiManager.UpdateHealth(health);
             if (health <= 0)
@@ -87,7 +128,6 @@ public class Player : MonoBehaviour
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
-       
     }
 
     void OnCollisionExit2D(Collision2D collision)
@@ -95,9 +135,7 @@ public class Player : MonoBehaviour
         if (collision.gameObject.GetComponent<Ground>() != null)
         {
             isGrounded = false;
-            animator.SetBool("Fall", true);
         }
-        
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -118,14 +156,12 @@ public class Player : MonoBehaviour
 
         if (collision.gameObject.GetComponent<Door>() != null)
         {
-            Debug.Log("У двери");
             collision.gameObject.GetComponent<Door>().TryOpenDoor(keysCollected);
         }
 
         if (collision.gameObject.GetComponent<Finish>() != null && keysCollected >= 5)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-                        
         }
     }
 }
